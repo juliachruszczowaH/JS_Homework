5c2e304511bc50edfd3715f3d14a998825269cd4
juliachruszczowaH <julia.chruszczowa@gmail.com>
2019-10-28 23:25:48 +0300

    fix/resolve_notes

--- hometask_8/hometask_8.js
+++ hometask_8/hometask_8.js
@@ -1,17 +1,18 @@
 function Animal(name) {
-    this._name = name;
-    this._foodAmount = 50;
-    this._formatFoodAmount = function () {
-        return (this._foodAmount + ' гр.');
+    this.name = name;
+    var foodAmount = 50;
+
+    var self = this;
+    function formatFoodAmount () {
+        return (foodAmount + ' гр.');
     }
 
-
-    this._feed = function () {
-        return ('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+    this.feed = function () {
+        console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
     };
 
     this.dailyNorm = function (amount) {
-        if (!arguments.length) return this._formatFoodAmount();
+        if (!arguments.length) return formatFoodAmount();
 
         if (amount < 50) {
             throw new Error("Значение должно быть больше 50");
@@ -20,24 +21,24 @@ function Animal(name) {
             throw new Error("Нельзя дать корма больше, чем 500 гр.");
         }
 
-        this._foodAmount = amount;
+        foodAmount = amount;
     };
 }
 
 function Cat(name) {
     Animal.apply(this, arguments);
 
-    var animalFeed = this._feed;
+    var animalFeed = this.feed;
     this.feed = function () {
-        console.log(animalFeed.apply(this) + '\nКот доволен ^_^');
+        animalFeed();
+        console.log('Кот доволен ^_^');
         return this;
-
-    }
+    };
 
     this.stroke = function () {
         console.log('Гладим кота.');
         return this;
-    }
+    };
 
 }
 
@@ -46,13 +47,14 @@ var barsik = new Cat('Barsik');
 console.log(barsik.name);
 
 console.log(barsik.dailyNorm());
-console.log(barsik.feed());
+barsik.feed();
 
-console.log(barsik.dailyNorm(600));
-console.log(barsik.feed());
-
-console.log(barsik.dailyNorm(250));
-console.log(barsik.feed());
-
+barsik.dailyNorm(100);
+barsik.feed();
 barsik.stroke().feed().stroke().stroke().feed();
+barsik.dailyNorm(350);
+console.log(barsik.dailyNorm());
+barsik.feed();
+
+barsik.feed().stroke().feed().feed().stroke().stroke().feed();
 barsik = null;
